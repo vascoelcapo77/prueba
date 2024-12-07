d13d5d39a4f851d1d17d9d2d6a01b8223c5c65c1
vascoelcapo77 <vascodanielq@gmail.com>
2024-12-06 20:27:16 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+meme_dict = {"ESTERNOCLEIDOMASTOIDEO": "músculo del cuello desde el esternón y la clavícula hasta la apófisis mastoides que interviene en los movimientos de flexión y giro de la cabeza","PARALELEPIPEDO": "sólido tridimensional con seis caras que son paralelogramos y cuyas caras opuestas son iguales y paralelas","OVIVIPARO","animales que se desarrollan en huevos que permanecen dentro del cuerpo de la madre hasta su eclosión"}
+            
+ word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")           
+if word in meme_dict.keys():
+    print(name_dict[word])
+    
+else:
+    print:("no existe esa palabra bro")
